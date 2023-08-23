4fd9239d655fbb9225c2223b0028b73db68b930b
bharathr18 <bharathr.16ec@saividya.ac.in>
2023-08-23 20:10:51 +0530

    Added User controller class in Controller package

---
+++ src/main/java/com/practice/backend/BookMyMovies/Service/UserService.java
@@ -0,0 +1,30 @@
+package com.practice.backend.BookMyMovies.Service;
+
+import com.practice.backend.BookMyMovies.Dtos.UserRequestDto;
+import com.practice.backend.BookMyMovies.Models.UserEntity;
+import com.practice.backend.BookMyMovies.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String createUser(UserRequestDto userRequestDto)
+    {
+        // Converting the dto to UserEntity for the repository class
+        UserEntity userEntity = UserEntity.builder().name(userRequestDto.getName()).mobile(userRequestDto.getMobile()).build();
+
+        try
+        {
+            userRepository.save(userEntity);
+        }
+        catch (Exception e)
+        {
+            return "user couldnt be added";
+        }
+        return "User added successfully";
+    }
+}
